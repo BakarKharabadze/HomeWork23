9450730ff2ecc1f0079cb8bc70b6528ac74e9c67
BakarKharabadze <kharabadzebaqar@gmail.com>
2024-05-26 12:30:29 +0400

    Initial Commit

---
+++ GroceryStoreApp/GroceryStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreAppApp.swift
+//  GroceryStoreApp
+//
+//  Created by Bakar Kharabadze on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
